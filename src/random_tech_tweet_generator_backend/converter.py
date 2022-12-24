800c7eb303e15812e9ee5c36d78165ffd6096f97
Roy, Debakar <debakar.roy@intel.com>
2022-12-24 11:21:05 +0530

    Move db.json to root

--- src/random_tech_tweet_generator_backend/converter.py
+++ src/random_tech_tweet_generator_backend/converter.py
@@ -4,14 +4,14 @@ from pathlib import Path
 from database import TweetDatabase
 
 if __name__ == "__main__":
-    db_root = Path(__file__).parent.parent.parent
+    root = Path(__file__).parent.parent.parent
     try:
-        db = TweetDatabase(db_root / "db" / "tweets.db")
+        db = TweetDatabase(root / "db" / "tweets.db")
         tweets = db.get_all()
     finally:
         db.close()
 
     json_data = {"metadata": {"entries": len(tweets)}, "tweets": tweets}
 
-    with open(db_root / "db" / "tweets.json", "w") as f:
+    with open(root / "db.json", "w") as f:
         json.dump(json_data, f, indent=4)
